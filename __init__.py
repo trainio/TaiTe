b4c711808ed6078a3abecb0321d282f498b0fd33
trainio <tuomo.rainio@gmail.com>
2021-03-09 18:39:33 +0200

    Kuvan lataaminen lisätty

--- __init__.py
+++ __init__.py
@@ -2,5 +2,6 @@
 from google.colab.patches import cv2_imshow as avaa_kuva
 
 # OPENCV
+from cv2 import imread as lataa_kuvatiedosto
 from cv2 import GaussianBlur as gaussin_sumennus
 from cv2 import medianBlur as mediaani_sumennus
